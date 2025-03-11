fa816ffa7f50a924233a5a1a743e44f81ba4eb97
Rafy Akbar <rafyakbar02@gmail.com>
2025-03-11 01:32:02 -0400

    added custom hook for fetching payees

---
+++ hooks/usePayees.ts
@@ -0,0 +1,34 @@
+import { supabase } from "@/lib/supabase";
+import { useEffect, useState } from "react"
+
+interface Payee {
+    payee: string
+}
+
+/**
+ * Fetch user payees, load to payeeList
+ */
+export function usePayees(userID: string | undefined) {
+    const [payeeList, setPayeeList] = useState<Payee[]>([])
+
+    useEffect(() => {
+        if (!userID)
+            return;
+
+        const fetchPayees = async () => {
+            const { data, error } = await supabase
+                .from('transactions')
+                .select('payee')
+                .eq('user_id', userID)
+
+            if (error)
+                throw error
+
+            setPayeeList(data)
+        }
+
+        fetchPayees()
+    }, [userID])
+
+    return payeeList
+}
